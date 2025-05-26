473dedc4f309858006ab648dce70ebc0823ea030
sunwoo202335248 <sunwoo2528@gachon.ac.kr>
2025-05-26 12:08:41 +0900

    Revert "Merge pull request #12 from nahy-512/sunwoo-v2"
    This reverts commit 8ecf148d79e6d245531c0a5de20f46188c4b34aa, reversing
    changes made to 2a75ef6ca36c5b737a055023b9cfcf2be30a483c.

---
+++ app/src/main/java/com/nahyun/mz/ui/trend/TrendFragment.kt
@@ -0,0 +1,12 @@
+package com.nahyun.mz.ui.trend
+
+import com.nahyun.mz.R
+import com.nahyun.mz.databinding.FragmentTrendBinding
+import com.nahyun.mz.ui.base.BaseFragment
+
+class TrendFragment : BaseFragment<FragmentTrendBinding>(R.layout.fragment_trend) {
+
+    override fun setup() {
+        // 뷰 초기화, ClickListener 정의 등 작업 진행
+    }
+}
